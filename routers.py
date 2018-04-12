ce81e2d71a35e3acf1983e8d937d5c5213d64e60
cdk1220 <donkuruppu@Dons-MacBook-Pro.local>
2018-04-12 11:56:39 -0500

    fixed the edge of a graph and figured out how to call the dijkstras function properly.

--- routers.py
+++ routers.py
@@ -1,15 +1,24 @@
 import dijkstras
+
+# Network layout of the routers as given in the project description
 graph = {
             'A': {'B': 4, 'C': 3, 'E': 7},
-            'B': {'A': 4, 'C': 5, 'L': 5},
+            'B': {'A': 4, 'C': 6, 'L': 5},
             'C': {'A': 3, 'B': 6, 'D': 11},
             'D': {'C': 11, 'L': 9, 'F': 6, 'G': 10},
             'E': {'A': 7, 'G': 5},
             'F': {'L': 5, 'D': 6},
             'G': {'E': 5, 'D': 10},
             'L': {'B': 5, 'D': 9, 'F': 5}
-            }
+        }
 
-shortest_path = dijkstras.dijkstra(graph,'A','F')
+# Calculating the shortest paths to and from
+pathAnnToJan = dijkstras.dijkstras(graph,'A','F', visited=[], distances={}, predecessors={})
+pathJanToAnn = dijkstras.dijkstras(graph,'F','A', visited=[], distances={}, predecessors={})
 
-print(shortest_path)
+print(pathAnnToJan)
+print(pathJanToAnn)
+
+
+
+
