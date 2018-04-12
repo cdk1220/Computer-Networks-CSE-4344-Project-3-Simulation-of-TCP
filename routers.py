57cd67e8ee75456869d24d75b0723bfeef3c42b9
cdk1220 <donkuruppu@Dons-MacBook-Pro.local>
2018-04-12 12:30:21 -0500

    Added dictionary that contain router names and their port numbers.

--- routers.py
+++ routers.py
@@ -2,15 +2,16 @@ import dijkstras
 
 # Network layout of the routers as given in the project description
 graph = {
-            'A': {'B': 4, 'C': 3, 'E': 7},
-            'B': {'A': 4, 'C': 6, 'L': 5},
-            'C': {'A': 3, 'B': 6, 'D': 11},
-            'D': {'C': 11, 'L': 9, 'F': 6, 'G': 10},
-            'E': {'A': 7, 'G': 5},
-            'F': {'L': 5, 'D': 6},
-            'G': {'E': 5, 'D': 10},
-            'L': {'B': 5, 'D': 9, 'F': 5}
-        }
+    'A': {'B': 4, 'C': 3, 'E': 7},
+    'B': {'A': 4, 'C': 6, 'L': 5},
+    'C': {'A': 3, 'B': 6, 'D': 11},
+    'D': {'C': 11, 'L': 9, 'F': 6, 'G': 10},
+    'E': {'A': 7, 'G': 5},
+    'F': {'L': 5, 'D': 6},
+    'G': {'E': 5, 'D': 10},
+    'L': {'B': 5, 'D': 9, 'F': 5}
+}
+
 
 # Calculating the shortest paths to and from
 pathAnnToJan = dijkstras.dijkstras(graph,'F','A', visited=[], distances={}, predecessors={})
@@ -22,15 +23,18 @@ pathChanToJan = pathJanToChan[::-1]
 pathAnnToChan = dijkstras.dijkstras(graph,'E','A', visited=[], distances={}, predecessors={})
 pathChanToAnn = pathAnnToChan[::-1]
 
-print(pathAnnToJan)
-print(pathJanToAnn)
-
-print(pathJanToChan)
-print(pathChanToJan)
-
-print(pathAnnToChan)
-print(pathChanToAnn)
-
+# Dictionary for router name and associated port number
+routerNameAndPort = {
+    'A': 8000,
+    'B': 8001,
+    'C': 8002,
+    'D': 8003,
+    'E': 8004,
+    'F': 8005,
+    'G': 8006,
+    'L': 8007,
+    'H': 8008
+}
 
 
 
